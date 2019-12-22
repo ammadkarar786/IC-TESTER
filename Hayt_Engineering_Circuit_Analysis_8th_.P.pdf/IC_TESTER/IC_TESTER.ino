087bf5d1f859725efe89fd3e320d1a49dad44a4f
ammadkarar <ammadkararuni@gmail.com>
2019-12-22 22:19:32 +0500

    XOR GATE CHECK

--- Hayt_Engineering_Circuit_Analysis_8th_.P.pdf/IC_TESTER/IC_TESTER.ino
+++ Hayt_Engineering_Circuit_Analysis_8th_.P.pdf/IC_TESTER/IC_TESTER.ino
@@ -8,9 +8,10 @@ void setup()
 
 void loop()
 {
-  AND();
-  OR();
-  NOR();
+//  AND();
+//  OR();
+//  NOR();
+//  XOR();
     }
 
 void AND(){
@@ -741,8 +742,185 @@ if (gate1==true && gate2==true && gate3==true &&gate4==true){
           Serial.println(gate_fail_pin[i]);
           }
     }
+    }}
+    void XOR(){
+  {
+  int gate_clear=0;
+  int gate_fail=0;
+  int gate_fail_pin[4];
+  int add=0;
+  //first gate/ //7486
+  //pin number
+  gate_fail=0;
+  gate_clear=0;
+  bool gate1;
+  int p3=3;
+   digitalWrite(13,1);
+   digitalWrite(12,1);
+  if( digitalRead(11)==0){/// first gate clear
+    gate_clear++;
+    digitalWrite(13,0);
+   digitalWrite(12,1);
+   if(digitalRead(11)==1){
+    gate_clear++;
+    digitalWrite(13,0);
+   digitalWrite(12,0);
+   if (digitalRead(11)==0){
+    Serial.println("XOR GATE 1 CLEAR PIN 1,2,3");
+    }else{
+      gate_fail++;
+      }
+   }else{
+    gate_fail++;
+      }
+   
     }
+    
+   else{  ////first gate not clear
+    gate_fail++;
+    Serial.println("First gate not clear 1,2,3");
+   }
+   if (gate_fail>0){
+    gate1=false;
+      
+      gate_fail_pin[add]=p3;
+    add++;
+}
+    else{
+    gate1=true;
+    }
+///checking of gate one ended
+//Second gate/ //7486
+  //pin number
+  gate_fail=0;
+  gate_clear=0;
+  bool gate2;
+  p3=6;
+   digitalWrite(10,1);
+   digitalWrite(9,1);
+  if( digitalRead(8)==0){/// 2nd gate clear
+    gate_clear++;
+    digitalWrite(10,0);
+   digitalWrite(9,1);
+   if(digitalRead(8)==1){
+    gate_clear++;
+    digitalWrite(10,0);
+   digitalWrite(9,0);
+   if (digitalRead(8)==0){
+    Serial.println("XOR GATE 2 CLEAR PIN 4,5,6");
+    }else{
+      gate_fail++;
+      }
+   }else{
+    gate_fail++;
+      }
+   
+    }
+    
+   else{  ////2nd gate not clear
+    gate_fail++;
+    Serial.println("XOR GATE 2 not clear PIN 4,5,6");
+   }
+   if (gate_fail>0){
+    gate2=false;
+    
+    gate_fail_pin[add]=p3;
+    add++;
+    }
+    else{
+    gate2=true;
+    }
+//third gate/ //7486
+  //pin number
+  p3=11;
+    gate_fail=0;
+  gate_clear=0;
+  bool gate3;
+   digitalWrite(7,1);
+   digitalWrite(6,1);
+  if( digitalRead(5)==0){/// 3rd gate clear
+    gate_clear++;
+    digitalWrite(7,0);
+   digitalWrite(6,1);
+    if(digitalRead(5)==1){
+    gate_clear++;
+    digitalWrite(7,0);
+   digitalWrite(6,0);
+   if (digitalRead(5)==0){
+    Serial.println("XOR GATE 3 CLEAR PIN 13,12,11");
+    }else{
+      gate_fail++;
+      }
+   }else{
+    gate_fail++;
+      }
+   
+    }
+    
+   else{  ////3rd gate not clear
+    gate_fail++;
+    Serial.println("3rd gate not clear 13,12,11");
+   }
+   if (gate_fail>0){
+    gate3=false;
+    gate_fail_pin[add]=p3;
+add++;
+    }
+    else{
+    gate3=true;
+    }
+///checking of gate 3rd ended
+    
+//4th gate/ //7486
+  //pin number
+  gate_fail=0;
+  gate_clear=0;
+  bool gate4;
+  p3=8;
+   digitalWrite(4,1);
+   digitalWrite(3,1);
+  if( digitalRead(2)==0){/// 4th gate clear
+    gate_clear++;
+    digitalWrite(4,0);
+   digitalWrite(3,1);
+   if(digitalRead(2)==1){
+    gate_clear++;
+    digitalWrite(4,0);
+   digitalWrite(3,0);
+     if (digitalRead(2)==0){
+    Serial.println("XOR GATE 4 CLEAR PIN 10,9,8");
+    }else{
+      gate_fail++;
+      }
+   }else{
+    gate_fail++;
+      }
+ 
+    }
+    
+   else{  ////4th gate not clear
+    gate_fail++;
+    Serial.println("4th gate not clear 10,9,8");
+   }
+   if (gate_fail>0){
+    gate4=false;
+    gate_fail_pin[add]=p3;
+    add++;
+}
+    if (gate_clear>0){
+   gate4=true;
+    }///checking of gate 4th ended
 
-  
-  
-  }
+if (gate1==true && gate2==true && gate3==true &&gate4==true){
+  Serial.println("XOR PASSES TEST");
+}else if(add==4){
+  Serial.println("Add gate test Fail");
+}
+  else{
+        Serial.println("list of fault gate pin");
+         
+         for (int i=0;i<add;i++){
+          Serial.println(gate_fail_pin[i]);
+          }
+    }
+    }}
